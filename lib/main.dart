2f7edc80dab4169beeb528e8bd252b085a3354c3
kirankamal <kiran.k@lucidplus.com>
2023-07-21 17:36:55 +0530

    refactor : theme updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:crud_app/features/home/presentation/home_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   await Hive.initFlutter();
-
+  await SystemChannels.textInput.invokeMethod('TextInput.hide');
   runApp(const MyApp());
 }
 
@@ -17,13 +24,30 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        useMaterial3: true,
-        scaffoldBackgroundColor: Colors.white,
-        appBarTheme: const AppBarTheme(backgroundColor: Colors.amberAccent),
-        floatingActionButtonTheme: const FloatingActionButtonThemeData(
-            backgroundColor: Colors.amberAccent),
-        cardTheme: const CardTheme(color: Colors.white),
-      ),
+          primarySwatch: Colors.amber,
+          useMaterial3: true,
+          scaffoldBackgroundColor: Colors.white,
+          appBarTheme: const AppBarTheme(
+              backgroundColor: Colors.amber,
+              iconTheme: IconThemeData(color: Colors.white)),
+          floatingActionButtonTheme: const FloatingActionButtonThemeData(
+              backgroundColor: Colors.amber),
+          cardTheme: const CardTheme(color: Colors.white),
+          bottomSheetTheme: const BottomSheetThemeData(
+            backgroundColor: Colors.white,
+          ),
+          // inputDecorationTheme: const InputDecorationTheme(
+          //     focusColor: Colors.amber,
+          //     focusedBorder: OutlineInputBorder(
+          //         borderSide: BorderSide(color: Colors.amber)),
+          //     activeIndicatorBorder: BorderSide(color: Colors.amber)),
+          datePickerTheme: const DatePickerThemeData(
+            backgroundColor: Colors.white,
+            surfaceTintColor: Colors.white,
+          ),
+          radioTheme: const RadioThemeData(
+              fillColor: MaterialStatePropertyAll(Colors.amber),
+              overlayColor: MaterialStatePropertyAll(Colors.amberAccent))),
       home: const HomeView(),
     );
   }
